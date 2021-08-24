b2ffe08294161f9d332d5b57ab6a28d2c661f796
MartinezYonathan <j-ma@hotmail.es>
2021-08-23 23:15:36 -0500

    se agrego el encabezado de eureka

--- src/main/java/mx/uacm/edu/incidencias/ServicioPersonasApplication.java
+++ src/main/java/mx/uacm/edu/incidencias/ServicioPersonasApplication.java
@@ -3,6 +3,7 @@ package mx.uacm.edu.incidencias;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ServicioPersonasApplication {
 
